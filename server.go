d4034bca76fdaa8710ad5d14905a35568eab0e23
Travis <mynameismyname1992@gmail.com>
2026-05-03 10:13:11 -0500

    Update handlers to use middlewareAuth

--- server.go
+++ server.go
@@ -35,31 +35,31 @@ func (s *Server) registerRoutes() {
 	s.mux.HandleFunc("POST /login", s.handleLoginUser)
 	s.mux.HandleFunc("POST /refresh", s.handleRefreshAccessToken)
 	s.mux.HandleFunc("POST /revoke", s.handleRevokeAccessToken)
-	s.mux.HandleFunc("PATCH /user/{id}/password", s.middlewareAuth(s.handleChangePassword))
-	s.mux.HandleFunc("PATCH /user/{id}/role", s.middlewareAuth(s.middlewareRequireRole("admin", s.handleUpdateUserRole)))
+	s.mux.HandleFunc("PATCH /users/{id}/password", s.middlewareAuth(s.handleChangePassword))
+	s.mux.HandleFunc("PATCH /users/{id}/role", s.middlewareAuth(s.middlewareRequireRole("admin", s.handleUpdateUserRole)))
 	s.mux.HandleFunc("GET /users", s.middlewareAuth(s.middlewareRequireRole("admin", s.handleGetAllUsers)))
 
-	s.mux.HandleFunc("GET /items", s.handleGetItems)
+	s.mux.HandleFunc("GET /items", s.middlewareAuth(s.handleGetItems))
 	s.mux.HandleFunc("POST /items", s.middlewareAuth(s.middlewareRequireRole("manager", s.handleCreateItem)))
-	s.mux.HandleFunc("GET /items/{id}", s.handleGetItemFromID)
+	s.mux.HandleFunc("GET /items/{id}", s.middlewareAuth(s.handleGetItemFromID))
 	s.mux.HandleFunc("DELETE /items/{id}", s.middlewareAuth(s.middlewareRequireRole("manager", s.handleDeleteItem)))
 	s.mux.HandleFunc("PATCH /items/{id}", s.middlewareAuth(s.middlewareRequireRole("manager", s.handleUpdateItem)))
 
 	s.mux.HandleFunc("POST /locations", s.middlewareAuth(s.middlewareRequireRole("manager", s.handleCreateLocation)))
-	s.mux.HandleFunc("GET /locations", s.handleGetLocations)
-	s.mux.HandleFunc("GET /locations/{id}", s.handleGetLocationFromID)
+	s.mux.HandleFunc("GET /locations", s.middlewareAuth(s.handleGetLocations))
+	s.mux.HandleFunc("GET /locations/{id}", s.middlewareAuth(s.handleGetLocationFromID))
 	s.mux.HandleFunc("PATCH /locations/{id}", s.middlewareAuth(s.middlewareRequireRole("manager", s.handleUpdateLocation)))
 	s.mux.HandleFunc("DELETE /locations/{id}", s.middlewareAuth(s.middlewareRequireRole("manager", s.handleDeleteLocation)))
 
-	s.mux.HandleFunc("GET /stock", s.handleGetAllStock)
-	s.mux.HandleFunc("GET /stock/alerts", s.handleGetLowStockItems)
-	s.mux.HandleFunc("GET /stock/item/{id}", s.handleGetItemStock)
-	s.mux.HandleFunc("GET /stock/location/{id}", s.handleGetLocationStock)
+	s.mux.HandleFunc("GET /stock", s.middlewareAuth(s.handleGetAllStock))
+	s.mux.HandleFunc("GET /stock/alerts", s.middlewareAuth(s.handleGetLowStockItems))
+	s.mux.HandleFunc("GET /stock/item/{id}", s.middlewareAuth(s.handleGetItemStock))
+	s.mux.HandleFunc("GET /stock/location/{id}", s.middlewareAuth(s.handleGetLocationStock))
 	s.mux.HandleFunc("POST /stock/receive", s.middlewareAuth(s.middlewareRequireRole("manager", s.handleReceiveStock)))
 	s.mux.HandleFunc("POST /stock/move", s.middlewareAuth(s.handleMoveStock))
 
 	s.mux.HandleFunc("GET /movements", s.middlewareAuth(s.middlewareRequireRole("manager", s.handleGetMovementHistory)))
-	s.mux.HandleFunc("GET /movements/item/{id}", s.handleGetItemMovementHistory)
+	s.mux.HandleFunc("GET /movements/item/{id}", s.middlewareAuth(s.middlewareRequireRole("manager", s.handleGetItemMovementHistory)))
 }
 
 func (s *Server) handleHealth(w http.ResponseWriter, r *http.Request) {
